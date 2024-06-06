603ba72525ccdfa684511769a853e25fbf713108
lyubenov1 <blyubenov9@gmail.com>
2024-06-06 13:44:24 +0300

    login and register views

--- src/main/java/com/techie/web/LoginController.java
+++ src/main/java/com/techie/web/LoginController.java
@@ -1,16 +1,17 @@
 package com.techie.web;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("login")
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login() {
+    @GetMapping
+    public String login(Model model, HttpServletRequest request) {
         return "login";
     }
-
 }
