6055255400f91fe063bcf7270ca3816ecacd1abd
lyubenov1 <blyubenov9@gmail.com>
2024-07-07 21:25:31 +0300

    On failed login implementation with redirect attributes (bad credentials).

--- src/main/java/com/techie/web/LoginController.java
+++ src/main/java/com/techie/web/LoginController.java
@@ -1,17 +1,27 @@
 package com.techie.web;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.web.authentication.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
 
 @Controller
-@RequestMapping("login")
+@RequestMapping
 public class LoginController {
 
-    @GetMapping
-    public String login(Model model, HttpServletRequest request) {
+    @GetMapping("/login")
+    public String login() {
         return "login";
+    }
+
+    @PostMapping("/login-error")
+    public String onFailedLogin(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+            RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username);
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+
+        return "redirect:/login";
     }
 }
