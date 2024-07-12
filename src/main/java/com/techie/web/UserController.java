9e3d460177c2121801621c7745d5214e9599d0f1
lyubenov1 <blyubenov9@gmail.com>
2024-07-12 19:19:36 +0300

    Several profile views base structure, styling and mapping.

--- src/main/java/com/techie/web/UserController.java
+++ src/main/java/com/techie/web/UserController.java
@@ -1,14 +1,46 @@
 package com.techie.web;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/users/profile")
 public class UserController {
 
-    @GetMapping("/profile")
+    @GetMapping
     public String getUserProfile() {
         return "profile";
     }
+
+    @GetMapping("/settings")
+    public String getUserSettings(Model model) {
+        model.addAttribute("activeMenuItem", "settings");
+        return "settings";
+    }
+
+    @GetMapping("/cart")
+    public String getUserCart(Model model) {
+        model.addAttribute("activeMenuItem", "cart");
+        return "cart";
+    }
+
+    @GetMapping("/wishlist")
+    public String getUserWishlist(Model model) {
+        model.addAttribute("activeMenuItem", "wishlist");
+        return "wishlist";
+    }
+
+    @GetMapping("/addresses")
+    public String getUserAddresses(Model model) {
+        model.addAttribute("activeMenuItem", "addresses");
+        return "addresses";
+    }
+
+    @GetMapping("/order-history")
+    public String getUserOrderHistory(Model model) {
+        model.addAttribute("activeMenuItem", "order-history");
+        return "order-history";
+    }
+
 }
