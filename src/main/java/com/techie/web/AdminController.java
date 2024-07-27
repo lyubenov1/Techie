fb6e2612287cc2974575d1ce388c3f13dad5cb5f
lyubenov1 <blyubenov9@gmail.com>
2024-07-27 10:37:21 +0300

    Added base structure and styling for the admin dashboard. Admin controller mappings

--- src/main/java/com/techie/web/AdminController.java
+++ src/main/java/com/techie/web/AdminController.java
@@ -1,15 +1,28 @@
 package com.techie.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @GetMapping
-    public String getAdminPage() {
-        return "admin";
+    public String getAdminPage(Model model) {
+        model.addAttribute("activeMenuItem", "dashboard");
+        return "admin/admin";
+    }
+
+    @GetMapping("/promotions")
+    public String getPromotionsPage(Model model) {
+        model.addAttribute("activeMenuItem", "promotions");
+        return "admin/promotions";
+    }
+
+    @GetMapping("/blacklist")
+    public String getBlacklistPage(Model model) {
+        model.addAttribute("activeMenuItem", "blacklist");
+        return "admin/blacklist";
     }
 }
